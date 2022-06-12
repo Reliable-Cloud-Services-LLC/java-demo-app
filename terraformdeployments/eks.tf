0c171a359321144127a64097b95c4a8d865f4550
Siham Darr <sdarr14@gmail.com>
2022-06-11 21:51:14 -0400

    more commits

---
+++ terraformdeployments/eks.tf
@@ -0,0 +1,30 @@
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "java-cluster"
+  cluster_version = "1.20"
+  vpc_id          = module.vpc.vpc_id
+ subnet_ids = module.vpc.private_subnets
+  
+}
+resource "null_resource" "java"{
+  depends_on = [module.eks]
+  provisioner "local-exec" {
+    command = "aws eks --region us-east-1  update-kubeconfig --name $AWS_CLUSTER_NAME"
+    environment = {
+      AWS_CLUSTER_NAME = "java-cluster"
+    }
+  }
+}
+
+
