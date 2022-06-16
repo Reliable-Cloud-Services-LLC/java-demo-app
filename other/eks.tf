b68cea7cca296bdb62bd1345328db8046642a3bb
Siham Darr <sdarr14@gmail.com>
2022-06-16 13:21:32 -0400

    moved provider

--- other/eks.tf
+++ other/eks.tf
@@ -5,6 +5,14 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
 
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+
+}
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "java-cluster"
@@ -14,12 +22,6 @@ module "eks" {
   
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-
-}
 resource "null_resource" "java"{
   depends_on = [module.eks]
   provisioner "local-exec" {
